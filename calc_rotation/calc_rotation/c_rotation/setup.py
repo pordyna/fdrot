99663b181b48bda5b673b642e8643dfad24875b2
Pawel Ordyna <pawel.ordyna@mailbox.tu-dresden.de>
2018-11-08 17:54:54 +0100

    include whole package

--- calc_rotation/calc_rotation/c_rotation/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-
-extensions = [Extension('rotation', ['rotation.pyx'])]
-setup(
-    ext_modules=cythonize(extensions, annotate=True, gdb_debug=False)
-)
---
+++ calc_rotation/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+extensions = [Extension('calc_rotation.c_rotation', ['calc_rotation/c_rotation.pyx'])]
+setup(name='Fdrot',
+    ext_modules=cythonize(extensions, annotate=True, gdb_debug=False),
+    packages = ['calc_rotation']
+)
