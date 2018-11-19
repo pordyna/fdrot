41ce63313d338a8a12ce5e945adf3b68b187ab77
Pawel Ordyna <pawel.ordyna@mailbox.tu-dresden.de>
2018-11-19 18:41:40 +0100

    tests

---
+++ calc_rotation/profile.py
@@ -0,0 +1,10 @@
+import pstats, cProfile
+
+import pyximport
+pyximport.install()
+
+
+cProfile.runctx("calc_pi.approx_pi()", globals(), locals(), "Profile.prof")
+
+s = pstats.Stats("Profile.prof")
+s.strip_dirs().sort_stats("time").print_stats()
--- calc_rotation/setup.py
+++ calc_rotation/setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 
-extensions = [Extension('calc_rotation.c_rotation', ['calc_rotation/c_rotation.pyx'])]
+
+extensions = [Extension('calc_rotation.c_rotation', ['calc_rotation/c_rotation.pyx'] )]
+                         # ,define_macros=[('CYTHON_TRACE', '1')])]
 setup(name='Fdrot',
-    ext_modules=cythonize(extensions, annotate=True, gdb_debug=False),
+    ext_modules=cythonize(extensions, annotate=True, gdb_debug=False, compiler_directives={'profile': True}),
     packages = find_packages(), version='0.0.2'
 )
+
