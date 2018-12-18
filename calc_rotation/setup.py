1aa9e2db7f64fb75da02871640e9262febc768a4
Pawel Ordyna <pawel.ordyna@mailbox.tu-dresden.de>
2018-12-18 12:40:38 +0100

    added requirements

--- calc_rotation/setup.py
+++ calc_rotation/setup.py
@@ -6,7 +6,9 @@ from Cython.Build import cythonize
 extensions = [Extension('fdrot.Kernel2D', ['fdrot/Kernel2D.pyx'])]
                          # ,define_macros=[('CYTHON_TRACE', '1')])]
 setup(name='Fdrot',
-      ext_modules=cythonize(extensions, annotate=True, gdb_debug=False,
+      ext_modules=cythonize(extensions,
+                            annotate=True, gdb_debug=False,
                             compiler_directives={'embedsignature': True, 'language_level': 3}),
+      install_requires=['numpy>=1.15', 'scipy', 'cython>=0.29'],
       packages=find_packages(), version='0.0.3')
 
