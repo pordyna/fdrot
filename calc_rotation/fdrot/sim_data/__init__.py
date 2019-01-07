cdd18e08fa933b4ef7319c6d6bba8c89b751d4d3
Pawel Ordyna <pawel.ordyna@mailbox.tu-dresden.de>
2019-01-07 14:53:47 +0100

    axis are now in a dictonary

--- calc_rotation/fdrot/sim_data/__init__.py
+++ calc_rotation/fdrot/sim_data/__init__.py
@@ -1,4 +1,4 @@
 from . import export_hdf5
-from .GenericList import GenericList, AxisOrder
+from .GenericList import GenericList
 # from .OpenPMDList import OpenPMDList
 from .UniversalSingle import UniversalSingle
