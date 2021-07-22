fix 3D calculation average cos^2 not the angle

fdrot/kernel3d.py
@@ -11,7 +11,7 @@ from numba import njit, prange
 # TODO hard copy signature
 
 
-@njit((numba.float64[::1], numba.float64[:, :, ::1], numba.float64[:, ::1],
+@njit((numba.float64[::1], numba.float64[:, :, ::1], numba.float64[:, :, ::1],
        numba.uint32, numba.uint32, numba.int32, numba.int32),
       parallel=True, cache=True)
 def kernel3d(pulse: np.ndarray,
@@ -27,9 +27,9 @@ def kernel3d(pulse: np.ndarray,
     Args:
         pulse: Pulse over which it's integrated.
         input_arr: B field in the right direction, already multiplied
-          with the electron density. First dimension is also the first
+          with the electron density. Second dimension is  the first
           axis in the output, 3rd axis is the  second  one in the
-          output; The effect is integrated  over the second axis.
+          output; The effect is integrated  over the first axis.
         output: The obtained rotation is added to it.
         global_start: global boundary to the left,the cell from which
           the propagation begins (This cell is also included).
@@ -51,39 +51,37 @@ def kernel3d(pulse: np.ndarray,
     pulse_head = leading_start
     # cell where the most left slice of the pulse is:
     pulse_tail = pulse_head - (pulse_len - 1)
-    for zz in prange(input_arr.shape[0]):
-        summed = np.zeros(input_arr.shape[2])  # y
-        input_slice = input_arr[zz, :, :]  # 2D (y,x)
-        for tt in prange(duration):
-            # Assigned variables in prange loops are private; pulse_head,
-            # pulse_tail can only be read here, so they stay global and
-            # available in the loop.
-            # cell where the most right slice of the pulse is:
-            pulse_head_private = pulse_head + tt
-            # cell where the most left slice of the pulse is:
-            pulse_tail_private = pulse_tail + tt
-            # Let's cut pulse on the global boundaries.
-            cut_at_tail = 0  # Cells cut at the pulse tail.
-            cut_at_head = 0  # Cells cut at the pulse head.
-            if pulse_tail_private < global_start:
-                cut_at_tail, pulse_tail_private = (global_start -
-                                                   pulse_tail_private), \
-                                                  global_start
-            if pulse_head_private > global_stop - 1:
-                cut_at_head, pulse_head_private = (pulse_head_private
-                                                   - (global_stop - 1)),\
-                                                  global_stop - 1
-            # Broadcasting arrays:
-            if cut_at_head == 0:
-                cut_pulse = pulse[cut_at_tail:]
-            else:
-                cut_pulse = pulse[cut_at_tail:-cut_at_head]
-            slice_chunk = input_slice[pulse_tail_private:pulse_head_private + 1
-                                      , :]  # Take all 'y' cut in 'x'.
-            # Faraday Rotation originating from the time interval [tt, tt+1].
-            # summed += ... : Shapes: (x,) * (x,y)  -> (y,) # prange reduction
-            slice_chunk = np.ascontiguousarray(slice_chunk)
-            summed += np.dot(cut_pulse, slice_chunk)
-            # propagate by one cell:
+    for tt in prange(duration):
+        # Assigned variables in prange loops are private; pulse_head,
+        # pulse_tail can only be read here, so they stay global and
+        # available in the loop.
+        # cell where the most right slice of the pulse is:
+        pulse_head_private = pulse_head + tt
+        # cell where the most left slice of the pulse is:
+        pulse_tail_private = pulse_tail + tt
+        # Let's cut pulse on the global boundaries.
+        cut_at_tail = 0  # Cells cut at the pulse tail.
+        cut_at_head = 0  # Cells cut at the pulse head.
+        if pulse_tail_private < global_start:
+            cut_at_tail, pulse_tail_private = (global_start -
+                                               pulse_tail_private), \
+                                              global_start
+        if pulse_head_private > global_stop - 1:
+            cut_at_head, pulse_head_private = (pulse_head_private
+                                               - (global_stop - 1)),\
+                                              global_stop - 1
+        # Broadcasting arrays:
+        if cut_at_head == 0:
+            cut_pulse = pulse[cut_at_tail:]
+        else:
+            cut_pulse = pulse[cut_at_tail:-cut_at_head]
+        field_chunk = input_arr[pulse_tail_private:pulse_head_private + 1
+                                  :, :]  # Take all 'y' cut in 'x'.
+        # Faraday Rotation originating from the time interval [tt, tt+1].
+        # summed += ... : Shapes: (x,) * (x,y)  -> (y,) # prange reduction
 
-        output[zz, :] += summed  # (y,) + (y,) # -zz -> zz # prange reduction
+        cut_pulse = np.expand_dims(np.expand_dims(cut_pulse, -1), -1)
+        if cut_at_head == 0:
+            output[cut_at_tail:, :, :] += cut_pulse * field_chunk
+        else:
+            output[cut_at_tail:-cut_at_head, :, :] += cut_pulse * field_chunk

fdrot/sim_sequence.py
@@ -12,11 +12,13 @@ import math
 import numpy as np
 from scipy.constants import (electron_mass, speed_of_light,
                              elementary_charge, epsilon_0)
+from numba import njit
 
 from .sim_data import GenericList
 from . import spliters
 from .Kernel2D import Kernel2D
 from .kernel3d import kernel3d
+from parallel_helpers import numba_multiply_arrays, average_over_pulse
 
 """
 This module is a part of the fdrot package.
@@ -201,10 +203,17 @@ class SimSequence:
                                 "cast to the desired type.".format(step))
         if transform is not None:
             data = transform(data)
+
+        # not sure if numba can parallelize copying but maybe it can
+        # so why not.
+        @njit(parallel=True)
+        def numba_contiguous_copy(array):
+            return np.ascontiguousarray(array)
+
         if make_contiguous:
             if not data.flags['C_CONTIGUOUS']:
                 # warn('The array was not C-contiguous.')
-                data = np.ascontiguousarray(data)
+                data = numba_contiguous_copy(data)
         return data
 
     def integration_factor(self, wavelength):
@@ -368,8 +377,8 @@ class SimSequence:
         assert len(last_axis) == 1
         last_axis = last_axis[0]
         # Set the desired axis order
-        desired_order = {self.propagation_axis: 1, second_axis_output: 2,
-                         last_axis: 0}
+        desired_order = {self.propagation_axis: 0, second_axis_output: 2,
+                         last_axis: 1}
         desired_order = AxisOrder(**desired_order)
         order_in_index = AxisOrder(**self.axis_map)
 
@@ -409,7 +418,7 @@ class SimSequence:
                             - dim_cut[output_second_idx][0])
 
         # Create output:
-        output = np.zeros((output_dim_0, output_dim_1), dtype=np.float64)
+        output = np.zeros((pulse.size, output_dim_0, output_dim_1), dtype=np.float64)
 
         # Begin calculation:
         for step in range(self.number_of_steps):
@@ -419,7 +428,10 @@ class SimSequence:
                                    dim_cut=dim_cut, cast_to=np.dtype('float64'))
             data_n = self.get_data('n_e', step, transform=transform,
                                    make_contiguous=True, dim_cut=dim_cut, cast_to=np.dtype('float64'))
-            data = data_b * data_n
+
+            data = numba_multiply_arrays(data_b, data_n)
+            del data_b
+            del data_n
             if include_relativistic_correction:
                 data_energy_density = self.get_data('energy_density', step, transform=transform,
                                                     make_contiguous=True, dim_cut=dim_cut, cast_to=np.dtype('float64'))
@@ -428,7 +440,12 @@ class SimSequence:
                 mean_energy = data_energy_density * cell_volume
                 if mean_energy_to_alpha is None:
                     mean_energy_to_alpha = _default_energy_to_alpha
-                data *= mean_energy_to_alpha(mean_energy)
+
+                @njit(parallel=True, cache=True)
+                def numba_mean_energy_to_alpha(array):
+                    return mean_energy_to_alpha(array)
+
+                data = numba_multiply_arrays(numba_mean_energy_to_alpha(mean_energy), data)
 
             step_interval = self.slices[step]
             local_start = 0
@@ -439,7 +456,9 @@ class SimSequence:
             kernel3d(pulse, data, output, local_start, local_end,
                      step_start, step_stop)
         output *= self.integration_factor(wavelength)
-        return output
+        output_flat = np.zeros((output_dim_0, output_dim_1), dtype=np.float64)
+        average_over_pulse(output, output_flat)
+        return output_flat
 
 
 def _get_params_and_check(files: GenericList, propagation_axis: str,